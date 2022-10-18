1d28f196b5307c60ccefc3f3dc1338e8c40a8586
Robinologist <112644607+Robinologist@users.noreply.github.com>
2022-10-18 10:27:20 -0400

    Wrote a function to create 6 hexagons surrounding another hexagon with user input for the size and color.

--- addition.py
+++ addition.py
@@ -1,9 +1,10 @@
 # Write your addition function in the space below...
-
+def add_two(num1,num2):
+    print("The sum of", num1, "and", num2, "is", int(num1)+int(num2))
 
 # Do not change anything below these lines
 def main():
-    pass
+    add_two(4,6)
 
 if __name__ == '__main__':
     main()
